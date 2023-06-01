5825f14524eeadcd2f4f5c1f7a220576cfd28ca6
Milla22 <marti.ludmilla@orange.fr>
2023-06-01 14:55:35 +0200

    transition collapses

--- src/components/Collapses/index.jsx
+++ src/components/Collapses/index.jsx
@@ -3,58 +3,83 @@ import Chevron from '../../Assets/images/chevron.png';
 
 function Collapses() {
     return (
-        <section>
-        <div className="fiabiliteSelect">
-        <span className ="fiabiliteOption1">
-            <p className ="pFiabiliteOption1">Fiabilté</p>
-                <img src= {Chevron} alt ="chevron" className ="chevron" />
-            </span>
-        <span className ="fiabiliteOption2">
-            <p className ="pFiabiliteOption2">Les annonces postées sur Kasa garantissent une 
-                                                                            fiabilité totale. Les photos sont conformes aux 
-                                                                            logements, et toutes les informations sont 
-                                                                            régulièrement vérifiée par nos équipes.</p>
-        </span>
-        </div>
-        <div className="respectSelect">
-        <span className ="respectOption1">
-            <p className ="pRespectOption1">Respect</p>
-            <img src= {Chevron} alt ="chevron" className ="chevron" />
-            </span>
-        <span className ="respectOption2">
-            <p className ="pRespectOption2">La bienveillence fait partie des valeurs fondatrices de Kasa. 
+        <section className="sectionCollapses">
+            <div className="wrap">
+            <input id="collapse1" className="toggle" type="checkbox"/>
+            <label for = "collapse1" className="labelToggle">
+                <p className="labelTitle">
+                Description
+                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
+                </p>
+                </label>
+            <div className="collapseText">
+                <div className="textInner">
+                    <p className="pText">
+                    Les annonces postées sur Kasa garantissent une 
+                    fiabilité totale. Les photos sont conformes aux 
+                    logements, et toutes les informations sont 
+                    régulièrement vérifiée par nos équipes.
+                    </p>
+                </div>
+            </div>
+            </div>
+            <div className="wrap">
+            <input id="collapse2" className="toggle" type="checkbox" />
+            <label for = "collapse2" className="labelToggle">
+            <p className="labelTitle">
+                Respect
+                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
+                </p>
+                </label>
+            <div className="collapseText">
+                <div className="textInner">
+                    <p className="pText">
+                    La bienveillence fait partie des valeurs fondatrices de Kasa. 
                 Tout comportement discrimminatoire ou de perturbation du voisinage 
                 entraînera une exclusion de notre plateforme.
-        </p>
-        </span>
-        </div>
-        <div className="serviceSelect">
-        <span className ="serviceOption1">
-            <p className ="pServiceOption1">Service</p>
-            <img src= {Chevron} alt ="chevron" className ="chevron" />
-            </span>
-        <span className ="serviceOption2">
-            <p className ="pServiceOption2">Nos équipes se tiennent à votre disposition pour vous fournir 
+                    </p>
+                </div>
+            </div>
+            </div>
+            <div className="wrap">
+            <input id="collapse3" className="toggle" type="checkbox" />
+            <label for = "collapse3" className="labelToggle">
+            <p className="labelTitle">
+                Service
+                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
+                </p>
+                </label>
+            <div className="collapseText">
+                <div className="textInner">
+                    <p className="pText">
+                    Nos équipes se tiennent à votre disposition pour vous fournir 
                 une expérience parfaite. Nhésitez pas à nous contacter si vous 
                 avez la moindre question.
-            </p>
-            </span>
-        </div>
-        <div className="securiteSelect">
-        <span className ="securiteOption1">
-            <p className ="pSecuriteOption1">Sécurité</p>
-            <img src= {Chevron} alt ="chevron" className ="chevron" />
-        </span>
-        <span className ="securiteOption2">
-            <p className ="pSecuriteOption2">La sécurité est la priorité de Kasa. Aussi bien pournos hôtes que pour 
+                    </p>
+                </div>
+            </div>
+            </div>
+            <div className="wrap">
+            <input id="collapse4" className="toggle" type="checkbox" />
+            <label for = "collapse4" className="labelToggle">
+            <p className="labelTitle">
+                Sécurité
+                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
+                </p>
+                </label>
+            <div className="collapseText">
+                <div className="textInner">
+                    <p className="pText">
+                    La sécurité est la priorité de Kasa. Aussi bien pournos hôtes que pour 
                 les voyageurs, chaque logement correspond aux critères de sécurité 
                 établis par nos services. En laissant une note aussi bien à l'hôte 
                 qu'au locataire, cela permet à nos équipes de vérifier que les 
                 standards sont bien respectés. Nous organisons également des ateliers 
                 sur la sécurité domestique pour nos hôtes.
-            </p>
-        </span>
-        </div>
+                    </p>
+                </div>
+            </div>
+            </div>
         </section>
     )
 }
