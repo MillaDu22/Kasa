1669f77639bca7975d2da2e1b051cb5302df2404
Milla22 <marti.ludmilla@orange.fr>
2023-06-01 16:29:16 +0200

    transition collapses LogementPage

--- src/components/Collapses/index.jsx
+++ src/components/Collapses/index.jsx
@@ -5,80 +5,80 @@ function Collapses() {
     return (
         <section className="sectionCollapses">
             <div className="wrap">
-            <input id="collapse1" className="toggle" type="checkbox"/>
-            <label for = "collapse1" className="labelToggle">
-                <p className="labelTitle">
-                Description
-                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
-                </p>
-                </label>
-            <div className="collapseText">
-                <div className="textInner">
-                    <p className="pText">
-                    Les annonces postées sur Kasa garantissent une 
-                    fiabilité totale. Les photos sont conformes aux 
-                    logements, et toutes les informations sont 
-                    régulièrement vérifiée par nos équipes.
+                <input id="collapse1" className="toggle" type="checkbox"/>
+                <label for = "collapse1" className="labelToggle">
+                    <p className="labelTitle">
+                        Description
+                        <img src = {Chevron} alt="chevronCollapse" className="chevron" />
                     </p>
+                </label>
+                <div className="collapseText">
+                    <div className="textInner">
+                        <p className="pText">
+                        Les annonces postées sur Kasa garantissent une 
+                        fiabilité totale. Les photos sont conformes aux 
+                        logements, et toutes les informations sont 
+                        régulièrement vérifiée par nos équipes.
+                        </p>
+                    </div>
                 </div>
-            </div>
             </div>
             <div className="wrap">
-            <input id="collapse2" className="toggle" type="checkbox" />
-            <label for = "collapse2" className="labelToggle">
-            <p className="labelTitle">
-                Respect
-                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
-                </p>
-                </label>
-            <div className="collapseText">
-                <div className="textInner">
-                    <p className="pText">
-                    La bienveillence fait partie des valeurs fondatrices de Kasa. 
-                Tout comportement discrimminatoire ou de perturbation du voisinage 
-                entraînera une exclusion de notre plateforme.
+                <input id="collapse2" className="toggle" type="checkbox" />
+                <label for = "collapse2" className="labelToggle">
+                    <p className="labelTitle">
+                        Respect
+                        <img src = {Chevron} alt="chevronCollapse" className="chevron" />
                     </p>
+                </label>
+                <div className="collapseText">
+                    <div className="textInner">
+                        <p className="pText">
+                            La bienveillence fait partie des valeurs fondatrices de Kasa. 
+                            Tout comportement discrimminatoire ou de perturbation du voisinage 
+                            entraînera une exclusion de notre plateforme.
+                        </p>
+                    </div>
                 </div>
-            </div>
             </div>
             <div className="wrap">
-            <input id="collapse3" className="toggle" type="checkbox" />
-            <label for = "collapse3" className="labelToggle">
-            <p className="labelTitle">
-                Service
-                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
-                </p>
-                </label>
-            <div className="collapseText">
-                <div className="textInner">
-                    <p className="pText">
-                    Nos équipes se tiennent à votre disposition pour vous fournir 
-                une expérience parfaite. Nhésitez pas à nous contacter si vous 
-                avez la moindre question.
+                <input id="collapse3" className="toggle" type="checkbox" />
+                <label for = "collapse3" className="labelToggle">
+                    <p className="labelTitle">
+                        Service
+                        <img src = {Chevron} alt="chevronCollapse" className="chevron" />
                     </p>
+                </label>
+                <div className="collapseText">
+                    <div className="textInner">
+                        <p className="pText">
+                            Nos équipes se tiennent à votre disposition pour vous fournir 
+                            une expérience parfaite. Nhésitez pas à nous contacter si vous 
+                            avez la moindre question.
+                        </p>
+                    </div>
                 </div>
-            </div>
             </div>
             <div className="wrap">
-            <input id="collapse4" className="toggle" type="checkbox" />
-            <label for = "collapse4" className="labelToggle">
-            <p className="labelTitle">
-                Sécurité
-                <img src = {Chevron} alt="chevronCollapse" className="chevron" />
-                </p>
-                </label>
-            <div className="collapseText">
-                <div className="textInner">
-                    <p className="pText">
-                    La sécurité est la priorité de Kasa. Aussi bien pournos hôtes que pour 
-                les voyageurs, chaque logement correspond aux critères de sécurité 
-                établis par nos services. En laissant une note aussi bien à l'hôte 
-                qu'au locataire, cela permet à nos équipes de vérifier que les 
-                standards sont bien respectés. Nous organisons également des ateliers 
-                sur la sécurité domestique pour nos hôtes.
+                <input id="collapse4" className="toggle" type="checkbox" />
+                <label for = "collapse4" className="labelToggle">
+                    <p className="labelTitle">
+                        Sécurité
+                        <img src = {Chevron} alt="chevronCollapse" className="chevron" />
                     </p>
+                </label>
+                <div className="collapseText">
+                    <div className="textInner">
+                        <p className="pText">
+                            La sécurité est la priorité de Kasa. Aussi bien pournos hôtes que pour 
+                            les voyageurs, chaque logement correspond aux critères de sécurité 
+                            établis par nos services. En laissant une note aussi bien à l'hôte 
+                            qu'au locataire, cela permet à nos équipes de vérifier que les 
+                            standards sont bien respectés. Nous organisons également des ateliers 
+                            sur la sécurité domestique pour nos hôtes.
+                        </p>
+                    </div>
                 </div>
-            </div>
             </div>
         </section>
     )
--- src/components/InfoLogement/index.jsx
+++ src/components/InfoLogement/index.jsx
@@ -6,48 +6,69 @@ function InfoLogement () {
     return (
         <section className="sectionInfo">
             <div className="rowBoxes1">
-            <div className="leftBox">
-                <h2 className ="title">Appartement cozy</h2>
-                <h3 className = "location">Paris, île de France</h3>
-                <div className=" containerMotsCles">
-                    <span className="motCle">Batignolle</span>
-                    <span className="motCle">Montmartre</span>
+                <div className="leftBox">
+                    <h2 className ="title">Appartement cozy</h2>
+                    <h3 className = "location">Paris, île de France</h3>
+                    <div className=" containerMotsCles">
+                        <span className="motCle">Batignolle</span>
+                        <span className="motCle">Montmartre</span>
+                    </div>
+                </div>
+                <div className="rightBox">
+                    <div className ="heberger">
+                        <span className="nameHeberger">Nathalie JEAN</span>
+                        <span className="photoHeberger">
+                            <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg" alt="profil" className="photoProfil" />
+                        </span>
+                    </div>
+                    <div className="containerNotation">
+                        <img src= {Rate} alt="notation" className="Rating" />
+                    </div>
                 </div>
             </div>
-            <div className="rightBox">
-                <div className ="heberger">
-                    <span className="nameHeberger">Nathalie JEAN</span>
-                    <span className="photoHeberger">
-                        <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/profile-picture-12.jpg" alt="profil" className="photoProfil" />
-                    </span>
+                <div className="rowBoxes2">
+                    <div className="wrapInfo">
+                        <input id="collapse5" className="toggleInfo" type="checkbox"/>
+                        <label for = "collapse5" className="labelToggleInfo">
+                            <p className="labelTitleInfo">
+                                Description
+                                <img src = {Chevron} alt="chevronCollapse" className="chevronInfo" />
+                            </p>
+                        </label>
+                        <div className="textInfo">
+                            <div className="textInnerInfo">
+                                <p className="pTextInfo">
+                                    Votre maison loin de chez vous. Que vous veniez de l'autre bout du monde, 
+                                    ou juste de quelques stations de RER, vous vous sentez chez vous dans 
+                                    notre appartement.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="wrapInfo">
+                        <input id="collapse6" className="toggleInfo" type="checkbox" />
+                        <label for = "collapse6" className="labelToggleInfo">
+                            <p className="labelTitleInfo">
+                                Equipements
+                                <img src = {Chevron} alt="chevronCollapse" className="chevronInfo" />
+                            </p>
+                        </label>
+                        <div className="textInfo">
+                            <div className="textInnerInfo">
+                                <p className="pTextInfo">
+                                    <nav className="listEquipement">
+                                        <ul className="ulList">
+                                            <li className= "liList">Equipements de base</li>
+                                            <li className= "liList">Micro-onde</li>
+                                            <li className= "liList">Douche italienne</li>
+                                            <li className= "liList">Frigo</li>
+                                            <li className= "liList">WIFI</li>
+                                        </ul>
+                                    </nav>
+                                </p>
+                        </div>
+                    </div>
                 </div>
-                <div className="containerNotation">
-                    <img src= {Rate} alt="notation" className="Rating" />
-                </div>
-            </div>
-            </div>
-            <div className="rowBoxes2">
-            <div className = "descriptionSelect">
-                <span className="descriptionOption1">Description<img src={Chevron} alt="collapse" className="chevronCollapse" /></span>
-                <span className=" descriptionOption2">Votre maison loin de chez vous. 
-                    Que vous veniez de l'autre bout du monde, ou juste de quelques 
-                    stations de RER, vous vous sentez chez vous dans notre appartement.
-                </span>
-            </div>
-            <div className = "equipementSelect">
-                <span className="equipementOption1">Equipements<img src={Chevron} alt="collapse" className="chevronCollapse" /></span>
-                <span className=" equipementOption2">
-                    <nav className="listEquipement">
-                        <ul className="ulList">
-                            <li className= "liList">Equipements de base</li>
-                            <li className= "liList">Micro-onde</li>
-                            <li className= "liList">Douche italienne</li>
-                            <li className= "liList">Frigo</li>
-                            <li className= "liList">WIFI</li>
-                        </ul>
-                    </nav>
-                </span>
-            </div>
             </div>
         </section>
     )
